7de941534d3778f7f89a337f2225fd70affd8128
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-08-06 16:49:08 -0400

    minor

---
+++ fix_es_watermark.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+curl -XPUT -H "Content-Type: application/json" http://localhost:9200/_cluster/settings -d '{ "transient": { "cluster.routing.allocation.disk.threshold_enabled": false } }'
+curl -XPUT -H "Content-Type: application/json" http://localhost:9200/_all/_settings -d '{"index.blocks.read_only_allow_delete": null}'

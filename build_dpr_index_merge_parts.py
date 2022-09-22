38a966a1dfd746c349d7f5fdbe581a9560d3b016
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-09-21 22:22:35 -0400

    minor

--- build_dpr_index_merge_parts.py
+++ build_dpr_index_merge_parts.py
@@ -18,15 +18,13 @@ def main():
             "hotpotqa", "strategyqa", "iirc", "2wikimultihopqa", "musique"
         )
     )
-    parser.add_argument("--num-shards", help="number of total shards", type=int, default=1)
+    parser.add_argument("--num-shards", help="number of total shards", type=int, required=True)
     args = parser.parse_args()
 
-    assert 0 <= args.shard_index < args.num_shards
-
     flat_index_path = os.path.join(WIKIPEDIA_CORPUSES_PATH, f"{args.dataset_name}-wikpedia-dpr-flat-index", "part_")
-
-    if not os.path.exists(flat_index_path):
-        exit(f"The flat_index_path (input/output) {flat_index_path} not available.")
+    for index in range(args.num_shards):
+        if not os.path.exists(flat_index_path + f"{index}"):
+            exit(f"The flat_index_path (input/output) {flat_index_path + f'{index}'} not available.")
 
     command = f"python -m pyserini.index.merge_faiss_indexes --prefix {flat_index_path} --shard-num {args.num_shards}"
     print("Running command:")

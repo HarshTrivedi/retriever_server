d308f6f25142d5dc9518f22f9646e14c12637218
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-11-24 15:43:52 -0500

    minor

---
+++ generate_contriever_embeddings.py
@@ -0,0 +1,15 @@
+import argparse
+from contriever_retriever import ContrieverRetriever
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="generate contriever embeddings")
+    parser.add_argument(
+        "dataset_name", type=str, help="dataset_name",
+        choices={"original", "musique_ans", "hotpotqa", "2wikimultihopqa", "iirc"}
+    )
+    args = parser.parse_args()
+
+    corpus_name = "musique" if args.dataset_name == "musique_ans" else args.dataset_name
+    ContrieverRetriever(corpus_name=corpus_name)

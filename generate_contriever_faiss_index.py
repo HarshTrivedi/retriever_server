927fe0fbc172fb61f67168b2630e3f3ffed1ca52
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-11-24 15:47:13 -0500

    minor

--- generate_contriever_faiss_index.py
+++ generate_contriever_faiss_index.py
@@ -7,7 +7,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="generate contriever embeddings")
     parser.add_argument(
         "dataset_name", type=str, help="dataset_name",
-        choices={"original", "musique_ans", "hotpotqa", "2wikimultihopqa", "iirc"}
+        choices={
+            "original", "musique_ans", "hotpotqa", "2wikimultihopqa", "iirc",
+            "chunked_musique_ans", "chunked_hotpotqa", "chunked_2wikimultihopqa", "chunked_iirc",
+        }
     )
     args = parser.parse_args()
 
